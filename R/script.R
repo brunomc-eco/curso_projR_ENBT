3e616edc291e4f08aa9b9dabecfdc5bf6470f32a
brunomc-eco <brunomc.eco@gmail.com>
2020-02-12 10:19:06 -0300

    nova funcao para ler a tabela no script e adicionando ultimos arquivos

--- R/script.R
+++ R/script.R
@@ -1,6 +1,5 @@
 # script para ler a tabela limpa
 # o dado original esta no arquivo ex04.xlsx
 
-data_format <- read.table(file = "data/ex04.csv",
-                          sep = ",",
-                          header = TRUE)
+data_format <- read.csv(file = "./data/ex04.csv")
+colnames(data_format) <- c("sp", "sep_comp", "sep_larg", "pet_comp", "pet_larg")
