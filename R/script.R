dfd530b1ef1d164f2a1a35415d8e27ab6e42a39e
brunomc-eco <brunomc.eco@gmail.com>
2020-02-12 10:37:00 -0300

    mudanca de nome de arquivos no script

--- R/script.R
+++ R/script.R
@@ -1,5 +1,5 @@
 # script para ler a tabela limpa
 # o dado original esta no arquivo ex04.xlsx
 
-data_format <- read.csv(file = "./data/ex04.csv")
+data_format <- read.csv(file = "./data/data_format.csv")
 colnames(data_format) <- c("sp", "sep_comp", "sep_larg", "pet_comp", "pet_larg")
